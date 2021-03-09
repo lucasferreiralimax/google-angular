32b4248f1670453d2e59503906e3718074f12f57
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-08 21:22:19 -0800

    adjustment test in workflow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'google-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('google-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('google-angular app is running!');
-  });
 });
