d79d579f16096f06ac29b64fef380a6e868419d7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-08 21:59:37 -0800

    addition component header and search

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
+import { SearchComponent } from '../../components/search/search.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    SearchComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
