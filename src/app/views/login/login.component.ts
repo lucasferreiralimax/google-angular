efcf2bd8baf4543fcb56f427f3093de44554e7dd
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-11 19:10:50 -0800

    adjustment login placeholder and show error empty

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm;
+  textInput: any = false;
+  error: any = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      user: ['lucasferreiralimax@gmail.com', [
+        Validators.required
+      ]]
+    })
   }
 
+  ngOnInit(): void {
+    this.textInput = this.loginForm.controls['user'].value
+  }
+
+  handleChange(e: any) {
+    this.textInput = e.target.value ? true : false;
+    this.error = !this.loginForm.controls['user'].value;
+  }
+
+  onSubmitLogin() {
+    this.error = !this.loginForm.controls['user'].value;
+  }
 }
--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
-
+import { LoginComponent } from './login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
