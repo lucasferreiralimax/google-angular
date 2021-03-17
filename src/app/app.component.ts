38cf204390393d839c20d705e8544597f1ac9c12
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-17 04:25:38 -0700

    adjustment page login style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import { DarkmodeService } from './services/darkmode.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
